44b9f5a99ca6ceb647f010c83f4b0a59ffa6766e
MikeBrost <mbros081@uottawa.ca>
2014-10-26 15:44:16 -0400

    MetricsResult comments
    comments for the new class MetricsResults

--- Iteration4/MetricsResult.py
+++ Iteration4/MetricsResult.py
@@ -1,4 +1,15 @@
 class MetricsResult:
+    """
+    Attributes:
+    startTime
+    endTime
+    detectionPercent: calculated using arguments length and numUndeteced
+    idChanges
+    minPositonalAccuracy
+    maxPositionalAccuracy
+    averagePositionalAccuracy: calculated using arguments totalDist, length and numUndeteced
+    percentWithinMaxRadius: calculated using arguments numAboveRadius, length and numUndeteced
+    """
     def __init__ (self, length, startTime, endTime, numUndetected, numIDChanges, minDist, maxDist, totalDist, numAboveRadius):
         self.startTime = startTime
         self.endTime = endTime
